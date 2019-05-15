b75473185d1a54ccab2dfbe9422e05a5c30e9047
Yibiao Yang <yangyibiao@outlook.com>
2019-05-15 22:13:51 +0800

    add

---
+++ branding.js
@@ -0,0 +1,23 @@
+function njuheader(){
+	document.write('<img src="rect-blank.png" border="0"><area shape="rect" coords="11,3,298,27" href="http://www.nju.edu.cn" alt="Nanjing University" target="_blank" /><area shape="rect" coords="794,7,828,25" href="http://www.nju.edu.cn" alt="NJU Home" target="_blank" /><area shape="rect" coords="835,7,873,25" href="http://cs.nju.edu.cn" alt="CS Department" target="_blank" /><area shape="rect" coords="880,7,916,25" href="http://www.nju.edu.cn" alt="CU A to Z" target="_blank"  /><area shape="rect" coords="925,7,949,25" href="http://www.nju.edu.cn/maps" alt="Campus Map" target="_blank" />');
+}
+
+// function njuheader(){
+//     document.write('<img src="rect-blank.png" border="0" usemap="#UCBMap" /><map name="UCBMap" id="UCBMap"><area shape="rect" coords="11,3,298,27" href="http://www.nju.edu.cn" alt="Nanjing University" target="_blank" /><area shape="rect" coords="794,7,828,25" href="http://www.nju.edu.cn" alt="NJU Home" target="_blank" /><area shape="rect" coords="835,7,873,25" href="http://cs.nju.edu.cn" alt="CS Department" target="_blank" /><area shape="rect" coords="880,7,916,25" href="http://www.nju.edu.cn" alt="CU A to Z" target="_blank"  /><area shape="rect" coords="925,7,949,25" href="http://www.nju.edu.cn/maps" alt="Campus Map" target="_blank" /></map>');
+// }
+
+function njufooter(){
+	document.write('<a target="_blank" href="http://cs.nju.edu.cn/"><img src="bg.jpg" alt="Nanjing University" width="200" /></a> &copy; Regents of the Nanjing University');
+}
+
+function navigation(){
+    document.write('<ul><li><a href="index.html">Home</a></li><li><a href="research.html">Publications</a></li></ul>'); 
+}
+
+function banner(){
+ document.write('<h1>Yibiao Yang</h1><p>Ph.D., Research Assistant Professor</p><p>Department of Computer Science and Technology, </p><p>Nanjing University, China</p><p>Email: yangyibiao(at)nju(dot)edu(dot)cn</p>');
+}
+
+function education(){
+    document.write('<h3>Education</h3><hr/>                Visiting Ph.D. in Computer Science: <br><a href="http://www.ucl.ac.uk">University College London</a> (UCL), London, UK. Oct 2013 ~ Dec 2013<br>                Research Supervisor: Prof. <a href="http://www.cs.ucl.ac.uk/staff/M.Harman">Mark Harman</a> and <a href="http://www.cs.ucl.ac.uk/staff/J.Krinke">Jens Krinke</a> (<a href="http://crest.cs.ucl.ac.uk">CREST</a> centre in <a href="http://www.cs.ucl.ac.uk">Department of Computer Science</a>)                <br><br>                Ph.D. in Computer Science: <br><a href="http://www.nju.edu.cn">Nanjing University</a> (NJU), Nanjing, China. Sep 2010 ~ Sep 2016<br>                Research Supervisor: Prof. Yuming Zhou                <br><br>                B.Sc. in Computer Software: <br><a href="http://www.swjtu.edu.cn">Southwest Jiaotong University</a> (SWJTU), Chengdu, China. Sep 2006 ~ Jun 2010                <br><br>');
+}
