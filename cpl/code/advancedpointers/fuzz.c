5d6687a1639da43eba1dcb288654fa4dc3a568df
yangyibiao <yangyibiao@nju.edu.cn>
2023-11-28 21:18:27 +0800

    update code for advanced pointers

--- cpl/code/advancedpointers/fuzz.c
+++ cpl/code/advancedpointers/fuzz.c
@@ -3,7 +3,7 @@
 
 int main() {
     // p is a pointer, the pointer is points to an array, and the array has two pointers
-    int *(*p)[2] = malloc(100); 
+    int *(*p)[2] = (int *)malloc(100 * sizeof(int)); 
     printf("%p\n", p);
     p++;
     printf("%p\n", p); // ???
--- cpl/code/advancedpointers/pointerto2arrays.c
+++ cpl/code/advancedpointers/pointerto2arrays.c
@@ -6,19 +6,21 @@
 void print_array(int a[][NUM_COLS], int rows, int cols)
 {
     int row, col;
-    for (row = 0; row < rows; row++) {
+    for (row = 0; row < rows; row++)
+    {
         for (col = 0; col < cols; col++)
             printf("%d ", a[row][col]);
         printf("\n");
     }
 }
 
-void print_array2(int **p, int rows, int cols)
+void print_array2(int *p, int rows, int cols)
 {
     int row, col;
-    for (row = 0; row < rows; row++) {
+    for (row = 0; row < rows; row++)
+    {
         for (col = 0; col < cols; col++)
-            printf("%d ", *(*(p+rows)+cols));
+            printf("%d ", *(p + rows * NUM_COLS + cols));
         printf("\n");
     }
 }
@@ -47,8 +49,8 @@ int main()
     printf("&a[1][1] = %p\n", &a[1][1]);
 
     /* 方法2: 将a视为一维的整型数组, 单个循环进行初始化 */
-    int * p;
-    for (p = &a[0][0]; p <= &a[NUM_ROWS-1][NUM_COLS-1]; p++) {
+    int *p;
+    for (p = &a[0][0]; p <= &a[NUM_ROWS - 1][NUM_COLS - 1]; p++) {
         *p = 1;
     }
 
@@ -56,7 +58,7 @@ int main()
     print_array(a, NUM_ROWS, NUM_COLS);
 
     /* 方法3: 使用指针数组来进行初始化 */
-    int * p2[NUM_ROWS];
+    int *p2[NUM_ROWS];
     for (row = 0; row < NUM_ROWS; row++) {
         p2[row] = &a[row][0];
     }
@@ -74,8 +76,8 @@ int main()
     printf("方法3: a = \n");
     print_array(a, NUM_ROWS, NUM_COLS);
 
-
-    /* 方法4: 使用指向指针的指针来进行初始化 */
+    /* 方法4: 使用指向指针的指针来进行初始化 (错误版本) */
+    /*
     int ** p3;
     p3 = &a[0];
     for (row = 0; row < NUM_ROWS; row++) {
@@ -86,80 +88,7 @@ int main()
 
     printf("方法4: a = \n");
     print_array(a, NUM_ROWS, NUM_COLS);
+    */
 
-
-    /* 方法5: 使用指向指针的指针来进行初始化 */
-    int ** p4;
-    p4 = a;
-    for (row = 0; row < NUM_ROWS; row++) {
-        for (col = 0; col < NUM_COLS; col++) {
-            p4[row][col] = 4;
-        }
-    }
-
-    printf("方法5: a = \n");
-    print_array(a, NUM_ROWS, NUM_COLS);
-
-    /* 方法6: 使用指向指针的指针来进行初始化 */
-    int ** p5;
-    p5 = a;
-    for (row = 0; row < NUM_ROWS; row++) {
-        for (col = 0; col < NUM_COLS; col++) {
-            *(*(p5 + row) + col) = 5;
-        }
-    }
-
-    printf("方法6: a = \n");
-    print_array(a, NUM_ROWS, NUM_COLS);
-
-
-    /* 方法7: 使用指向指针的指针来进行初始化 */
-    int ** p6;
-    p6 = a;
-    for (row = 0; row < NUM_ROWS; row++) {
-        for (col = 0; col < NUM_COLS; col++) {
-            *(p6[row] + col) = 6;
-        }
-    }
-
-    printf("方法7: a = \n");
-    print_array(a, NUM_ROWS, NUM_COLS);
-
-
-    /* 方法8: 使用指向指针的指针来进行初始化 */
-    int ** p7;
-    p7 = a;
-    for (row = 0; row < NUM_ROWS; row++) {
-        for (col = 0; col < NUM_COLS; col++) {
-            *(a[row] + col) = 7;
-        }
-    }
-
-    printf("方法8: a = \n");
-    print_array(a, NUM_ROWS, NUM_COLS);
-
-    /* 方法10: 使用指向指针的指针来进行初始化 */
-    int ** p9;
-    p9 = a;
-    for (row = 0; row < NUM_ROWS; row++) {
-        for (col = 0; col < NUM_COLS; col++) {
-            *(*(a + row) + col) = 9;
-        }
-    }
-
-    printf("方法10: a = \n");
-    print_array(a, NUM_ROWS, NUM_COLS);
-
-
-    /* 方法13: 使用指向指针的指针来进行初始化 */
-    int ** p12;
-    p12 = a;
-    for (row = 0; row < NUM_ROWS; row++) {
-        for (col = 0; col < NUM_COLS; col++) { 
-            *(p12[row] + col) = 11;
-        }
-    }
-
-    
+    return 0;
 }
-
