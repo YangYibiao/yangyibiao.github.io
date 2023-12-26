3510114e9113db26ac98f981930d5a82624c559c
yangyibiao <yangyibiao@nju.edu.cn>
2023-12-26 09:53:01 +0800

    Update selfstruct.c

--- cpl/code/LinkedList/selfstruct.c
+++ cpl/code/LinkedList/selfstruct.c
@@ -4,26 +4,26 @@
 //  float c;
 //};
 
+// struct SELF {
+//   int a;
+//   struct SELF b; // error: field ‘b’ recursively requires itself to be defined
+//   float c;
+// };
+
 struct SELF {
-  int a;
-  struct SELF b;
-  float c;
+ int a;
+ struct SELF * b;
+ float c;
 };
 
-//struct SELF {
+// typedef struct {
 //  int a;
-//  struct SELF * b;
+//  SELF * b; // error: SELF undeclared (first use in this function)
 //  float c;
-//};
+// } SELF;
 
-typedef struct {
+typedef struct SELF_TAG {
  int a;
- SELF * b;
+ struct SELF_TAG *b;
  float c;
 } SELF;
-
-//typedef struct SELF_TAG {
-//  int a;
-//  struct SELF_TAG *b;
-//  float c;
-//} SELF;
