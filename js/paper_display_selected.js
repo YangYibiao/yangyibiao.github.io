f7005000383bc708a8a1fbb6d3fe29ae7bfd207c
Yibiao Yang <yangyibiao@outlook.com>
2025-05-05 00:23:24 +0800

    Update paper_display_selected.js

--- js/paper_display_selected.js
+++ js/paper_display_selected.js
@@ -47,15 +47,12 @@ window.onload = function() {
                 var meta_str = meta_array.join('  /  ')
 
                 var bgcolor_str = "'#ffffff'"
-                // if (highlight == true) {
-                //     bgcolor_str = "'#ffffd0'"
-                // }
                 html += 
                 "\
                 <tr bgcolor=" + bgcolor_str + ">\
                     <td width='60%' valign='middle'>\
                     <p>\
-                        <a href=" + url + " style='font-size:18px'>" + title + "</a>\
+                        <a href='" + url + "' style='font-size:18px'>" + title + "</a>\
                         <br>" + author_str + 
                         "<br>\
                         <em>"+ publisher + ",  " + year + " (" + "<span style='color: red;'>" + tier + "</span>" + ")" + equal_str + "</em>\
