4ff3df330591add3e8d957483f1bc4204006217b
Yibiao Yang <yangyibiao@nju.edu.cn>
2025-10-30 20:34:42 +0800

    Update prime.c

--- cpl/slides/2-if-for-array/oj-code/prime.c
+++ cpl/slides/2-if-for-array/oj-code/prime.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-int main() {
-  long long n;
-  scanf("%lld", &n);
+int is_prime[50000000] = {0};
 
-  int sum = 1;
-  for (long long i = 3; i <= n; i = i + 2) {
-    if (i > 10 && (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0)) {
-      continue;
-    }
-    int prime = 1;
-    for (long long j = 2; j * j <= i; j++) {
-      if (i % j == 0) {
-        prime = 0;
-        break;
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  is_prime[2] = 1;
+  for (int i = 3; i <= n; i+=2) {
+    is_prime[i] = 1;
+  }
+
+  // i * i <= n 说明 i <= sqrt(n)
+  for (int i = 2; i * i <= n; ++i) {
+    if (is_prime[i]) {
+      for (int j = i * i; j <= n; j += i) {
+        is_prime[j] = 0;
       }
     }
-    if (prime) {
-      sum++;
-    }
+  }
+
+  int sum = 1;
+  for (int i = 3; i <= n; i += 2) {
+    if (is_prime[i]) sum++;
   }
   printf("%d", sum);
   return 0;
