616c073b658faf9626c72d77517304dfc08e4dd5
Yibiao Yang <yangyibiao@nju.edu.cn>
2025-11-01 10:09:34 +0800

    Update guess.c

--- cpl/slides/0-intro/code/guess.c
+++ cpl/slides/0-intro/code/guess.c
@@ -1,17 +1,33 @@
-#include <stdio.h>
+#include <stdio.h> // 标准输入输出库文件/库函数
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    int number = 5;
-    int guess;
+int main()
+{
+    srand(time(NULL));// 设置随机种子
+    int rand_number = rand();
+    printf("rand_number: %d\n", rand_number);
+    int target = rand_number % 100 + 1; // 保证要猜的数一定在1 到 100之间。
 
-    printf("Guess a number: ");
-    scanf("%d", &guess);
+    int i = 1;
+    while (i <= 5) {
+        int guess;
+        printf("(The %dth try)Please guess a number between 1 to 100:", i);
 
-    if (guess == number) {
-        printf("You guessed it!\n");
-    } else {
-        printf("You didn't guess it.\n");
+        scanf("%d", &guess);
+        if (guess == target) {
+            printf("You win!\n");
+            return 0;
+        }
+
+        if (guess > target) {
+            printf("target is smaller than %d\n", guess);
+        } else {
+            printf("target is greater than %d\n", guess);
+        }
+        i = i + 1;
     }
+    printf("You lose!");
 
     return 0;
 }
