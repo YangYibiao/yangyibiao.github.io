50606fc1a8a68998048c1805e00881fc6327a60d
Yibiao Yang <yangyibiao@nju.edu.cn>
2025-11-27 13:02:27 +0800

    Update paper_display_selected.js

--- js/paper_display_selected.js
+++ js/paper_display_selected.js
@@ -23,7 +23,7 @@ window.onload = function() {
                     continue;
                 }
 
-                if(tier.search("ATC") != -1 || tier.search("ASPLOS") != -1 || tier.search("OOPSLA") != -1) {
+                if(tier.search("POPL") != -1 || tier.search("PLDI") != -1 || tier.search("SOSP") != -1 || tier.search("OSDI") != -1 || tier.search("ASPLOS") != -1 || tier.search("ATC") != -1 || tier.search("OOPSLA") != -1) {
                     tier = "<b>"+tier+"</b>"
                 }
 
